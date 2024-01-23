b315ae56875cbd996a61ef5b051ce8cd4815eaa2
Kevin Koech <kevinkoech@proton.me>
2024-01-23 02:39:29 -0800

    add function that returns the list of school having a specific topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+Define function that returns the list of school having a specific topic.
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    function that returns the list of school having a specific topic.
+    """
+    topic_filter = {
+        'topics': {
+            '$elemMatch': {
+                '$eq': topic,
+            },
+        },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
