f42aa0a88d6514516f873ded7441b17d533aba6b
Kevin Koech <kevinkoech@proton.me>
2024-01-24 14:13:41 +0300

    Check 4 correction

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -21,7 +21,7 @@ class Cache:
         Initialize the class.
         """
         self._redis = redis.Redis()
-        self._redis.flushdb(True)
+        self._redis.flushdb()
 
     def store(self, data: Union[int, str, float, bytes]) -> str:
         """
